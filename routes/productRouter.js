2307b45eea2b65a4d0d0fee0c3a6fdd6cb53b5d8
Roman Baranovskii <rom8614@gmail.com>
2024-04-29 17:44:53 +0300

    server upd3

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const productController = require('../controllers/productController');
+const checkRole = require("../middleware/RoleMiddleware");
 
 const router = new Router();
 
-router.post('/', productController.create);
+router.post('/', checkRole('ADMIN'), productController.create);
 router.get('/', productController.getAll);
 router.get('/:id', productController.getOne);
 
